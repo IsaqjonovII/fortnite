1ca078b6e3d5b4e5d643a2f0324dd2cc6154f0ac
ilhomjon003 <ilhomjonisoqjonov2@gmail.com>
2023-01-04 11:08:00 +0500

    banner

--- src/components/banner/Banner.jsx
+++ src/components/banner/Banner.jsx
@@ -1,9 +1,9 @@
-import React, { useState, useEffect, useRef } from "react";
+import React from "react";
 import c from "./Banner.module.css";
 import fortnite from "../../assets/fortnite.svg"
 
 const Banner = () => {
-  
+
 
   return (
     <div className={c.banner}>
@@ -11,64 +11,24 @@ const Banner = () => {
         <img src="https://cdn2.unrealengine.com/zero-build-battle-royale-logo-blue-en-2739x1246-955d0fb099dc.png" alt="" />
       </div>
 
-      <div className={c.center_container}>
-        <div className={c.left}>
-          <p>june 23, 2022</p>
-          <h2>fortnite x naruto shippuden: raise your flag invitatio... </h2>
-          <img
-            src="https://cdn2.unrealengine.com/21br-narutopt2-4-characters-newsheader-1900x600-6dc4934c34c2.jpg"
-            alt=""
-          />
+      <div className={c.grid__contanier}>
+        <div className={c.grid__item}>
+          <img src="https://cdn2.unrealengine.com/23br-c4s1-brweaponsandfeatures-motd-1920x1080-1920x1080-5eb245d5d8ce.jpg" alt="" />
         </div>
-
-        <div className={c.right}>
-          <p>june 21, 2022</p>
-          <h2>the nindo 2022: complete challenges to earn naruto rew...</h2>
-          <img
-            src="https://cdn2.unrealengine.com/fortnite-the-nindo-1900x600-1900x600-cf6a616310f2.png"
-            alt=""
-          />
+        <div className={c.grid__item}>
+          <img src="https://cdn2.unrealengine.com/january-fortnite-crew-pack-with-gildhart-thumbnail-576x576-31e7e0b331a9.jpg" alt="free pass" />
         </div>
-      </div>
-      <div className={c.flex}>
-        <div className={c.flex_boxes}>
-          <img
-            src="https://cdn2.unrealengine.com/21br-narutopt2-4-characters-newsheader-1900x600-6dc4934c34c2.jpg"
-            alt=""
-          />
-          <p>june 21, 2022</p>
-          <h2>naruto's rivals join team 7 in fortnite</h2>
+        <div className={c.grid__item}>
+          <img src="https://cdn2.unrealengine.com/fortnite-giannis-antetokounmpo-thumbnail-400x400-c72621be0225.jpg" alt="free pass" />
         </div>
-        <div className={c.flex_boxes}>
-          <img
-            src="https://cdn2.unrealengine.com/13stw-hittheroadloading-motd-1920x1080-1920x1080-116968bac3e6.jpg"
-            alt=""
-          />
-          <p>june 21, 2022</p>
-          <h2>save the world v21.10 homebase status report</h2>
+        <div className={c.grid__item}>
+          <img src="https://cdn2.unrealengine.com/23br-mha-keyart-social-newsthumb-576x576-0c949b61fdd6.jpg" alt="free pass" />
         </div>
-        <div className={c.flex_boxes}>
-          <img
-            src="https://cdn2.unrealengine.com/fortnite-island-hopper-1900x600-127c6d0e1ef6.jpg"
-            alt=""
-          />
-          <p>june 21, 2022</p>
-          <h2>Earn in game rewards with fortnite island hopper q...</h2>
+        <div className={c.grid__item}>
+          <img src="https://cdn2.unrealengine.com/fortnite-reboot-rally-thumbnail-576x576-72ff0a2857f8.jpg" alt="free pass" />
         </div>
       </div>
 
-      <div className={c.bottom_container}>
-        <img
-          src="https://cdn2.unrealengine.com/battle-pass-promo-footer-bg-desktop-1920x1228-c2021f528166.png"
-          alt=""
-          className={c.banner_img}
-        />
-        <img src={fortnite} alt="" className={c.logo} />
-        <h2>Chapter 3 - Season 3</h2>
-        <img src="https://cdn2.unrealengine.com/vibin-no-shadow-black-en-1520x484-72250831462d.png" alt="" className={c.png} />
-        {/* <h2>Battle pass</h2> */}
-        <button className={c.valid_btn}>available  now!</button>
-      </div>
     </div>
   );
 };
